67fccff62cd635e6bc49e079d5dbb6c11eab21ea
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2015-05-26 15:59:52 +1200

    scaffolded out basic frontend index

---
+++ index.js
@@ -0,0 +1,3 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiZW52aW50YWdlIiwiYSI6Inh6U0p2bkEifQ.p6VrrwOc_w0Ij-iTj7Zz8A';
+var map = L.mapbox.map('map', 'envintage.i9eofp14')
+    .setView([-41.28646, 174.77624], 12);
